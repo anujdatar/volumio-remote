906a3a1818c633fac91ba4cefa66fe692f3c5c96
Anuj Datar <anuj.datar@gmail.com>
2019-01-11 09:48:55 -0500

    WIP - implementing webengine

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include <QtWebEngine/qtwebengineglobal.h>
+
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     QApplication a(argc, argv);
+
+    QtWebEngine::initialize();
+
     MainWindow w;
     w.show();
 
---
+++ mainwindow.qml
@@ -0,0 +1,8 @@
+
+import QtQuick 2.2
+import QtQml 2.2
+import QtWebEngine 1.7
+
+ApplicationWindow {
+
+}
